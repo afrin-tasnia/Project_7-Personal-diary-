77a865fcb0c5066b1a1aed460d7837a2fb9de1ff
Md Abul Bashar Nirob <88394238+ABnirob@users.noreply.github.com>
2025-08-11 20:31:21 +0600

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,22 @@
 // main.c
+#include "diary.h"
+#include <locale.h>
+#include <windows.h>
+
+// Extended color definitions
+#define COLOR_RED       12
+#define COLOR_GREEN     10
+#define COLOR_YELLOW    14
+#define COLOR_BLUE       9
+#define COLOR_MAGENTA   13
+#define COLOR_CYAN      11
+#define COLOR_WHITE     15
+#define COLOR_PURPLE     5
+#define COLOR_LIME      10
+#define COLOR_TEAL       3
+#define COLOR_ORANGE     6
+#define COLOR_PINK      13
+#define COLOR_GRAY       8  // Added for backup option
+
 
 
