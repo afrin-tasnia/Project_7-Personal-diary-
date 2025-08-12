f66dc3562ce226654da4ab347ba3258d966542e2
Md Abul Bashar Nirob <88394238+ABnirob@users.noreply.github.com>
2025-08-12 13:46:25 +0600

    Update diary.h

--- diary.h
+++ diary.h
@@ -1,22 +1,21 @@
-// diary.h
 #ifndef DIARY_H
 #define DIARY_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
+#include <stdio.h>    
+#include <stdlib.h>   
+#include <string.h>   
 
 // Function Declarations
-void displayMenu();
-void addEntry();
-void viewEntries();
-void searchEntry();
-void editEntry();
-void deleteEntry();
-void encryptFile();
-void decryptFile();
-void backupDiary();  
+void displayMenu();     // Displays the main menu options
+void addEntry();        // Adds a new diary entry
+void viewEntries();     // Displays all diary entries
+void searchEntry();     // Searches for a specific entry by date
+void editEntry();       // Edits an existing diary entry
+void deleteEntry();     // Deletes a diary entry
+void encryptFile();     // Encrypts the diary file
+void decryptFile();     // Decrypts the diary file
+void backupDiary();     // Creates a backup of the diary file
 
 #endif
+
 
